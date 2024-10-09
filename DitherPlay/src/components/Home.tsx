01222654e7654496b68cc7a37486b390926ba298
PrajwalDhule <prajwaldhule36@gmail.com>
2024-10-09 22:52:35 +0530

    branch-main: added product hunt embed link

--- DitherPlay/src/components/Home.tsx
+++ DitherPlay/src/components/Home.tsx
@@ -623,6 +623,19 @@ function Home() {
               >
                 Reset
               </Button>
+
+              <a
+                href="https://www.producthunt.com/posts/ditherplay?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-ditherplay"
+                target="_blank"
+              >
+                <img
+                  src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=496197&theme=dark"
+                  alt="DitherPlay - Realtime&#0032;image&#0032;dithering&#0032;&#0038;&#0032;filters&#0032;in&#0032;a&#0032;responsive&#0032;interface | Product Hunt"
+                  width="250"
+                  height="54"
+                  className="lg:hidden w-[187px] h-[40px] mt-8"
+                />
+              </a>
             </div>
           </div>
         </div>
